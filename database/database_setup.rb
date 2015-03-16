f283f7899cfb5a3c201208243b87daec3711e04e
Andrew Yolland <and.t.yoll@gmail.com>
2015-03-16 15:31:56 -0500

    trying to fix Gemfile...

--- database/database_setup.rb
+++ database/database_setup.rb
@@ -1,16 +1,3 @@
-# arr = []
-# 16.times do |y|
-#   16.times do |x|
-#     arr << "x" + x.to_s + "y" + y.to_s + " INTEGER"
-#   end
-# end
-#
-# sql_str=  "CREATE TABLE IF NOT EXISTS sprites (id INTEGER PRIMARY KEY, name TEXT, #{arr.join(', ')})"
-#
-# DATABASE.execute(sql_str)
-
-# DATABASE = SQLite3::Database.new("sprites.db")
-# DATABASE.results_as_hash = true
 
 configure :development do
   require "sqlite3"
