440e12cee4eadf27209dd7270b4fe2f2ce25ce35
Andrew Yolland <and.t.yoll@gmail.com>
2015-03-18 10:26:43 -0500

    whoops fixed javascript goof

--- public/draw.js
+++ public/draw.js
@@ -1,6 +1,6 @@
 function boot(){
   palette = ["rgb(212, 235, 138)","rgb(153, 181, 102)","rgb(95, 128, 67)","rgb(26, 61, 24)","rgba(255, 0, 255, 0)"];
-  xHR = new XMLHttpRequest();/Users/andrewyolland/Desktop/KirbyTileset.gif
+  xHR = new XMLHttpRequest();
   highSwatch = document.getElementById("highlight");
   midSwatch = document.getElementById("midtone");
   shadSwatch = document.getElementById("shadow");
